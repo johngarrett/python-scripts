77997bf14a7d708a0cf26e34a3933f419083bac1
jgarrett <jgarrett@kabbage.com>
2019-12-02 14:29:15 -0500

    refactor

--- phys.py
+++ phys.py
@@ -3,25 +3,20 @@ from queue import PriorityQueue
 test_grades = PriorityQueue()
 overall_grade = 0
 
+print("Input percentage grades as whole numbers (e.g. 82.4).\n")
 for i in range(1,5):
-    test_grades.put(float(input(f"Enter grade for test {i}: ")))
+    test_grades.put(float(input(f"Enter percentage for test {i}: ")))
 
 
 for weight in [0.06, 0.09, 0.12, 0.18]:
     overall_grade +=  test_grades.get() * weight
 
-overall_grade += float(input("Enter average lab grade: ")) * .15
-overall_grade += float(input("Enter average recitation grade: ")) * .05
-overall_grade += float(input("Enter average practice homework grade: ")) * .04
-overall_grade += float(input("Enter average class participation grade: ")) * .04
-overall_grade += float(input("Enter average regular homework grade: ")) * .05 #todo: check
-overall_grade += float(input("Enter average reading questions grade: ")) * .02
+for (title, weight) in [("lab", .15), ("recitation", .05),
+        ("practice homework", .04), ("class participation", .04),
+        ("regular homework", .05), ("reading quesitons", .02)]: 
+    overall_grade += float(input(f"Enter average {title} grade: ")) * weight
 # todo: bonus points
 
-# A
-print("TO get an A: \n")
-print((90 - overall_grade) / 0.25)
-print("TO get a B: \n")
-print((80 - overall_grade) / 0.25)
-print("TO get a C: \n")
-print((70 - overall_grade) / 0.25)
+#tood: check regular homeworkg rade precentage
+for grade in range(90, 69, -10):
+    print(f"\n To get a {grade}, you need a: \n {(grade - overall_grade) / 0.25}% on the final")
